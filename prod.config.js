c74aeb7523ba4a7ead4884c123503eff15df9110
sifelkateb <sifelkateb@gmail.com>
2021-10-14 20:02:40 +0200

    basic webpack setup
    setup production and development config
    use content hashing for cach busting
    initialize project structure in src folder
    use a simple file app.js and views/index.html for testing functionality

---
+++ prod.config.js
@@ -0,0 +1,54 @@
+const path= require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports={
+  entry:'./src/client/app.js',
+  mode:'production',
+  output:{
+    path:path.resolve(__dirname,'dist'),
+    filename:'[name].[contenthash].js',
+    assetModuleFilename: 'images/[name].[contenthash].[ext]',
+    libraryTarget: 'var',
+    library: 'Client'
+  },
+  optimization:{
+    minimizer:[new CssMinimizerPlugin(),new TerserPlugin()],
+ },
+  plugins:[
+    new HtmlWebpackPlugin({
+      template:'./src/client/views/index.html',
+      filename: 'index.[contenthash].html'
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename:'style/style.[contenthash].css'
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread']
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use:[MiniCssExtractPlugin.loader,"css-loader","sass-loader"]
+      }
+    ]
+  }
+}
