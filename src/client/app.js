c6ccc26b8c92b50e08870dc11e7781369844c149
sifelkateb <sifelkateb@gmail.com>
2021-10-16 02:10:05 +0200

    update clientside code
    create 2 modules
    first module for checking if url are valid or not
    second module for  updating UI depending on the responseData from server

--- src/client/app.js
+++ src/client/app.js
@@ -2,17 +2,16 @@ import './styles/style.scss';
 //reference solve the generator runtime error!
 //https://dev.to/hulyakarakaya/how-to-fix-regeneratorruntime-is-not-defined-doj
 import 'regenerator-runtime/runtime';
-
+import {updataUi} from './js/update_ui.js'
+import {isURL} from './js/checkurl.js'
 const input= document.querySelector('#input');
 const btn= document.querySelector('#btn');
-const scoreTag= document.querySelector('#scoretag');
-const agreement= document.querySelector('#agreement');
-const subjectivity=document.querySelector('#subjectivity');
-const confidence=document.querySelector('#confidence');
-const irony =document.querySelector('#irony');
-const results=document.querySelector('#results');
+/*
+1-post url input by the user to the server
+2-fetch the response from the server
+3- update the UI accordingly
+*/
 const postData=async(url,obj)=>{
-
   try{
   const response = await fetch(url, {
     method: 'POST', 
@@ -33,18 +32,15 @@ const postData=async(url,obj)=>{
 /*
 update UI depending on the results from the postData async function
 */
-const updataUi=(responseData)=>{
-  results.style.display='table';
-  agreement.innerHTML=responseData.agreement;
-  subjectivity.innerHTML=responseData.subjectivity;
-  irony.innerHTML=responseData.irony;
-  confidence.innerHTML=responseData.confidence;
-  scoreTag.innerHTML=responseData.score_tag;
-}
 
 // add event listener to the btn and call the postData function with the url provided by the users
 btn.addEventListener('click',()=>{
+
   const articleUrl=input.value;
+  if(!isURL(articleUrl)){
+    alert('please enter a valid url of the article you want to analyze');
+    return;
+  }
   const obj={articleUrl};
   postData('http://localhost:8081/sendurl',obj);
 });
---
+++ src/client/js/update_ui.js
@@ -0,0 +1,14 @@
+export const updataUi=(responseData)=>{
+  const scoreTag= document.querySelector('#scoretag');
+  const agreement= document.querySelector('#agreement');
+  const subjectivity=document.querySelector('#subjectivity');
+  const confidence=document.querySelector('#confidence');
+  const irony =document.querySelector('#irony');
+  const results=document.querySelector('#results');
+  results.style.display='table';
+  agreement.innerHTML=responseData.agreement;
+  subjectivity.innerHTML=responseData.subjectivity;
+  irony.innerHTML=responseData.irony;
+  confidence.innerHTML=responseData.confidence;
+  scoreTag.innerHTML=responseData.score_tag;
+}
